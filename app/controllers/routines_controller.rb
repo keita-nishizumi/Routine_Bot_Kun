f7a60158aef8c08669fab3f4f684febba382b4ab
Keita Mac <ktnshzm@gmail.com>
2020-09-27 08:44:47 +0900

    added create routine func

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -3,4 +3,18 @@ class RoutinesController < ApplicationController
     # TODO: 後からログインしているユーザのルーティン一覧のみ取得するように改変する
     @routines = Routine.all
   end
+
+  def new
+    @routine = Routine.new
+  end
+
+  def create
+    @routine = Routine.new(routine_params)
+    @routine.save
+  end
+
+  private
+def routine_params
+    params.require(:routine).permit(:name, :description).merge(user_id: current_user.id)
+  end
 end
--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,2 +1,4 @@
 class Routine < ApplicationRecord
+  belongs_to :user
+  has_many :users, through: :routine_users
 end
