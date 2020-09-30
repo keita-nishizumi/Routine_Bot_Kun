ddc0501590b670c1c5b0e357ea3825e449973c70
Keita Mac <ktnshzm@gmail.com>
2020-09-30 16:06:21 +0900

    added check items table

--- db/migrate/20200929233439_create_check_items.rb
+++ db/migrate/20200929233439_create_check_items.rb
@@ -1,7 +1,8 @@
 class CreateCheckItems < ActiveRecord::Migration[6.0]
   def change
     create_table :check_items do |t|
-
+      t.string :text, null: false
+      t.references :check_list, null:false, foreign_key: true
       t.timestamps
     end
   end
