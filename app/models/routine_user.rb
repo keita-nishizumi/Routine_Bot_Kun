e0364b01e208065d7cd17272196d6bc6e4df109a
Keita Mac <ktnshzm@gmail.com>
2020-09-30 21:14:17 +0900

    display check items from database

--- app/models/routine_user.rb
+++ app/models/routine_user.rb
@@ -3,6 +3,6 @@ class RoutineUser < ApplicationRecord
   belongs_to :routine
 
   def self.fetch_todays_routines(current_user)
-    RoutineUser.where("user_id = ? AND NOW() BETWEEN start_date AND end_date", current_user).includes(:routine).order("time ASC")
+    RoutineUser.where("user_id = ? AND NOW() BETWEEN start_date AND end_date", current_user).includes(:routine => {:check_list => :check_items}).order("time ASC")
   end
 end
